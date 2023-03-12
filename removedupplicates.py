fbc6db8d18651d4c21584fd4fe378fdf517e5eeb
Vishal <84004536+vishal160719@users.noreply.github.com>
2023-03-12 15:16:57 +0530

    Create removedupplicates.py

---
+++ removedupplicates.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = list(set(nums))[:]
+        nums.sort()
+        return(len(nums))
